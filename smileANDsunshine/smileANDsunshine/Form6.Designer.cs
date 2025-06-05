d1d475efe34035dc10ec9ebc2c4614a36bd1e9b0
Shahkar Hussain <shahkarhssn@gmail.com>
2025-06-05 18:22:15 +0800

    33

--- smileANDsunshine/smileANDsunshine/Form6.Designer.cs
+++ smileANDsunshine/smileANDsunshine/Form6.Designer.cs
@@ -34,7 +34,7 @@ namespace smileANDsunshine
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(431, 37);
+            this.button1.Location = new System.Drawing.Point(582, 105);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
